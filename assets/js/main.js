829cabf72a14be91412fac45455f5a9971ad2a25
Lobov Evgeniy <lobov.em@gmail.com>
2023-01-04 13:00:17 +0100

    Burger is ready, next step

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log('2322Test');
+// управление меню
+const menuBtn = document.querySelector('.navbar__burger');
+const menuList = document.querySelector('.navbar__list');
+const body = document.querySelector('body');
+
+menuBtn.addEventListener('click', (e) => {
+  menuBtn.classList.toggle('active');
+  body.classList.toggle('active');
+
+  if (menuList.classList.contains('active')) {
+    setTimeout(() => menuList.classList.toggle('active'), 500);
+  } else {
+    menuList.classList.toggle('active');
+  }
+});
