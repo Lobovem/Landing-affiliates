95079a63d6a06d3f6ea884c729b63ec20e5f306c
Lobov Evgeniy <lobov.em@gmail.com>
2023-01-09 19:56:30 +0100

    Работает тестовій слайд

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -63,3 +63,69 @@ const swiper = new Swiper('.swiper', {
   //   el: '.swiper-scrollbar',
   // },
 });
+
+const swiper2 = new Swiper('.swiper2', {
+  spaceBetween: 20,
+  // enabled: true,
+  // breakpoints: {
+  //   576: {
+  //     enabled: false,
+  //     slidesPerView: 3,
+  //     speed: 800,
+  //   },
+  // },
+
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination2',
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
+  },
+
+  //Количество слайдов на странице
+
+  // slidesPerView: 3,
+
+  //Адаптив
+
+  // breakpoints: {
+  //   310: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 10,
+  //   },
+  //   400: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20,
+  //   },
+
+  //   768: {
+  //     slidesPerView: 4,
+  //   },
+
+  //   1000: {
+  //     slidesPerView: 5,
+  //   },
+
+  //   1300: {
+  //     slidesPerView: 6,
+  //   },
+  // },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
