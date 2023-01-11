7e009eff3f38d345aa41df3a0afc9d2335bdb890
Lobov Evgeniy <lobov.em@gmail.com>
2023-01-11 16:51:37 +0100

    Сделал плавный переход по страницу на клик ссылки

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,11 @@ menuBtn.addEventListener('click', (e) => {
   navbar.classList.toggle('active');
   body.classList.toggle('active');
 });
+
+$(document).ready(function () {
+  $('.navbar__link').click(function (event) {
+    $('.navbar').removeClass('active');
+    $('.navbar__burger').removeClass('active');
+    $('.body').removeClass('active');
+  });
+});
