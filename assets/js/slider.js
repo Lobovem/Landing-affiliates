e51849bfdd74b5789b415ef93ef7508b0dc9602c
Lobov Evgeniy <lobov.em@gmail.com>
2023-01-11 14:54:48 +0100

    Ремонт faq

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -8,8 +8,6 @@ const swiper = new Swiper('.swiper3', {
       speed: 800,
     },
   },
-  // Optional parameters
-  // direction: 'vertical',
   loop: true,
 
   // If we need pagination
@@ -21,61 +19,11 @@ const swiper = new Swiper('.swiper3', {
   autoplay: {
     delay: 3000,
   },
-
-  //Количество слайдов на странице
-
-  // slidesPerView: 3,
-
-  //Адаптив
-
-  // breakpoints: {
-  //   310: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 10,
-  //   },
-  //   400: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 20,
-  //   },
-
-  //   768: {
-  //     slidesPerView: 4,
-  //   },
-
-  //   1000: {
-  //     slidesPerView: 5,
-  //   },
-
-  //   1300: {
-  //     slidesPerView: 6,
-  //   },
-  // },
-
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
 
 const swiper2 = new Swiper('.swiper2', {
   spaceBetween: 20,
-  // enabled: true,
-  // breakpoints: {
-  //   576: {
-  //     enabled: false,
-  //     slidesPerView: 3,
-  //     speed: 800,
-  //   },
-  // },
 
-  // Optional parameters
-  // direction: 'vertical',
   loop: true,
 
   // If we need pagination
@@ -87,46 +35,6 @@ const swiper2 = new Swiper('.swiper2', {
   autoplay: {
     delay: 3000,
   },
-
-  //Количество слайдов на странице
-
-  // slidesPerView: 3,
-
-  //Адаптив
-
-  // breakpoints: {
-  //   310: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 10,
-  //   },
-  //   400: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 20,
-  //   },
-
-  //   768: {
-  //     slidesPerView: 4,
-  //   },
-
-  //   1000: {
-  //     slidesPerView: 5,
-  //   },
-
-  //   1300: {
-  //     slidesPerView: 6,
-  //   },
-  // },
-
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
 
 const swiper3 = new Swiper('.swiper', {
